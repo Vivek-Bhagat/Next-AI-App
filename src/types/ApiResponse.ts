180c7d181fd431cacab947808a4c17ba419fc512
Vivek-Bhagat <vivekbhagat1890@hotmail.com>
2024-08-11 23:59:19 +0530

    api response email template

---
+++ src/types/ApiResponse.ts
@@ -0,0 +1,8 @@
+import { Message } from "@/model/User.model"
+
+export interface ApiResponse {
+    success: Boolean
+    message: string
+    isAcceptingMessage?: Boolean
+    messages?:Array<Message>
+}
